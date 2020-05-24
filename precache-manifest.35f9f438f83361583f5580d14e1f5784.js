ca59589058c9e3995684a20924692702c64f85b7
caspersweeb <viktor.barian@gmail.com>
2020-05-24 15:01:33 +0200

    Updates

---
+++ precache-manifest.35f9f438f83361583f5580d14e1f5784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f999f07c89d54cb3c8d7d7afaf17fcca",
+    "url": "/react-gol/index.html"
+  },
+  {
+    "revision": "8026d94a0aa765c504c9",
+    "url": "/react-gol/static/css/main.e6f452c9.chunk.css"
+  },
+  {
+    "revision": "775cb871b2201331a2ae",
+    "url": "/react-gol/static/js/2.170e6451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gol/static/js/2.170e6451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8026d94a0aa765c504c9",
+    "url": "/react-gol/static/js/main.e3952646.chunk.js"
+  },
+  {
+    "revision": "7949ef50021cd754bef2",
+    "url": "/react-gol/static/js/runtime-main.c35b8401.js"
+  }
+]);
